5adc076e9511dfbacb65751c6fbe5586da57211c
Gert Van Gool <gertvangool@gmail.com>
2012-01-02 14:53:38 +0100

    Added version information

--- advanced_reports/__init__.py
+++ advanced_reports/__init__.py
@@ -1,3 +1,19 @@
+#: The version list
+VERSION = (1, 3, 2)
+
+
+def get_version():
+    '''
+    Converts the :attr:`VERSION` into a nice string
+    '''
+    if len(VERSION) > 3 and VERSION[3] != 'final' and VERSION[3] != '':
+        return '%s.%s.%s %s' % (VERSION[0], VERSION[1], VERSION[2], VERSION[3])
+    else:
+        return '%s.%s.%s' % (VERSION[0], VERSION[1], VERSION[2])
+
+
+#: The actual version number, used by python (and shown in sentry)
+__version__ = get_version()
 
 REGISTRY = {}
 
