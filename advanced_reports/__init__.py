10b252d211dc77425bf2fd2a0b8dff569a02076f
Ingo Berben <ingoberben@gmail.com>
2012-12-18 11:05:23 +0100

    fix from Jef: don't cache the instance

--- advanced_reports/__init__.py
+++ advanced_reports/__init__.py
@@ -25,16 +25,17 @@ __version__ = get_git_version()
 REGISTRY = {}
 
 def register(advreport):
-    REGISTRY[advreport.slug] = advreport()
+    REGISTRY[advreport.slug] = advreport
 
 def get_report_for_slug(slug):
-    return REGISTRY.get(slug, None)
+    return REGISTRY.get(slug, lambda: None)()
 
 def get_report_or_404(slug):
     advreport = get_report_for_slug(slug)
     if advreport is None:
         from django.http import Http404
         raise Http404('No AdvancedReport matches the given query.')
+
     advreport.internal_mode = False
     advreport.report_header_visible = True
     return advreport
