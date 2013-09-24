da0c50fdadb823a95e0234406d39d13306a92f89
Jef Geskens <jef.geskens@gmail.com>
2013-09-24 14:56:42 +0200

    Removed bootstrap dependency

---
+++ advanced_reports/backoffice/shortcuts.py
@@ -0,0 +1,4 @@
+from advanced_reports.defaults import action as original_action
+
+class action(original_action):
+    form_template='advanced_reports/backoffice/contrib/advanced-reports/bootstrap-form.html'
